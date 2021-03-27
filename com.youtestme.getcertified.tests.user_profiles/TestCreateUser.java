3c49d3fba74596c5fdb0fbe2b75553432b1968ee
Katarina Rakicevic <53061439+katarinarakicevic@users.noreply.github.com>
2021-03-27 22:56:08 +0100

    Create TestCreateUser.java

---
+++ com.youtestme.getcertified.tests.user_profiles/TestCreateUser.java
@@ -0,0 +1,12 @@
+package com.youtestme.getcertified.tests.user_profiles;
+
+import org.testng.annotations.Test;
+
+import com.youtestme.getcertified.core.BaseTest;
+import com.youtestme.getcertified.core.pagehelpers.UserProfilesPageTestHelper;
+
+public class TestCreateUser extends BaseTest {
+  
+  
+  
+}
