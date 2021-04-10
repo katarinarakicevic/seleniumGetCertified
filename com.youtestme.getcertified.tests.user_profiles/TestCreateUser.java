539558d3346689a29928ad83ef0469f937e4d65c
Katarina Rakicevic <53061439+katarinarakicevic@users.noreply.github.com>
2021-04-10 19:59:12 +0200

    Update TestCreateUser.java

--- com.youtestme.getcertified.tests.user_profiles/TestCreateUser.java
+++ com.youtestme.getcertified.tests.user_profiles/TestCreateUser.java
@@ -6,7 +6,33 @@ import com.youtestme.getcertified.core.BaseTest;
 import com.youtestme.getcertified.core.pagehelpers.UserProfilesPageTestHelper;
 
 public class TestCreateUser extends BaseTest {
-  
-  
-  
+	@Test
+	public void test3CreateUserProfile() throws Exception {
+
+		UserProfilesPageTestHelper profilesPageTestHelper = super.factory.createUserProfilesPageHelper();
+
+		System.out.println("\n==== Start testing class TestCreateUser... ====");
+
+		application.login().openUserProfilesPage();
+		profilesPageTestHelper.pressNewUserPanel();
+		//profilesPageTestHelper.basicInfo();
+		// use this only in case if it is not checked by default
+		//profilesPageTestHelper.updatePicture();
+		
+		profilesPageTestHelper.typeUsername("test001");
+		profilesPageTestHelper.selectPasswordGenerated();
+		profilesPageTestHelper.selectRole();
+		profilesPageTestHelper.selectStatusActive();
+		profilesPageTestHelper.typeFirstName("Test");
+		profilesPageTestHelper.typeLastName("User");
+		profilesPageTestHelper.typeEmail("usertester1512@gmail.com");
+		profilesPageTestHelper.PressCreateUser();
+		Thread.sleep(5000);
+
+		application.logout();
+		
+		
+
 }
+}
+
