3e8af5b35eddea5b8a740eb4e9ff134ab8718b52
Katarina Rakicevic <53061439+katarinarakicevic@users.noreply.github.com>
2021-04-10 20:00:53 +0200

    Update UserProfilesPage.java

--- com.youtestme.getcertified.core.pages/UserProfilesPage.java
+++ com.youtestme.getcertified.core.pages/UserProfilesPage.java
@@ -1,5 +1,7 @@
 package com.youtestme.getcertified.core.pages;
+
 import org.openqa.selenium.By;
+
 
 /**
  * This class will contain convenient constants for elements inside login page.
@@ -11,27 +13,38 @@ public class UserProfilesPage {
 	 *
 	 */ 
 	public static final By UPDATE_PROFILE_PICTURE = By.xpath("//*[@id=\"newForm:j_idt422_label\"]");
+	
 	public static final By REMOVE_PROFILE_PICTURE = By.xpath("//*[@id=\"newForm:j_idt424\"]/span[1]");
-	public static final By BASIC_INFO_ONLY = By.xpath("//*[@id=\"newForm:j_idt427\"]/div[2]");
-	public static final By INPUT_NEW_USERNAME = By.id("newForm:j_idt431_content");
+	public static final By BASIC_INFO_ONLY = By.className("ui-toggleswitch-slider");
+	public static final By INPUT_NEW_USERNAME = By.xpath("//*[@id=\"newForm:j_idt431_display\"]");
+	public static final By SELECT_PASSWORD= By.xpath("//*[@id=\"newForm:options\"]/div[3]/span");
+	public static final By SELECT_PASSWORD_GENERATED=By.xpath("//*[@id=\"newForm:options_0\"]");
+	public static final By SELECT_PASSWORD_MANUALLY=By.xpath("//*[@id=\"newForm:options_1\"]");
 	public static final By ENTER_PASSWORD = By.id("newForm:password");
 	public static final By CONFIRM_PASSWORD = By.id("newForm:passConf");
 	public static final By PASSWORD_EXPIRES= By.className("ui-inplace-display");
+	public static final By SELECT_ROLE= By.xpath("//*[@id=\"newForm:roles\"]/div[3]/span");
+	public static final By SELECT_ROLE_ADMIN=By.xpath("//*[@id=\"newForm:roles_1\"]");
+	public static final By SELECT_USER_STATUS= By.xpath("//*[@id=\"newForm:status\"]/div[3]/span");
+	public static final By SELECT_USER_STATUS_ACTIVE= By.xpath("//*[@id=\"newForm:status_0\"]");
+	public static final By SELECT_USER_STATUS_SUSPENDED = By.xpath("//*[@id=\"newForm:status_1\"]");
+	public static final By SELECT_USER_STATUS_LOCKED=By.xpath("//*[@id=\"newForm:status_2\"]");
 	public static final String BASE_LANGUAGE = "newForm:language";
-	public static final By INPUT_NEW_FIRST_NAME = By.id("newForm:firstName");
-	public static final By INPUT_NEW_LAST_NAME = By.id("newForm:lastName");
+	public static final By INPUT_NEW_FIRST_NAME = By.xpath("//*[@id=\"newForm:j_idt482_display\"]");
+	public static final By INPUT_NEW_LAST_NAME = By.xpath("//*[@id=\"newForm:j_idt491_display\"]");
 	public static final By INPUT_NEW_BIRTH_DATE = By.id("newForm:birth_input");
 	public static final String BASE_RADIO_GENDER_ID = "newForm:gen";
 	public static final By INPUT_SEMESTER = By.id("newForm:semester_input");
 	public static final By INPUT_YEAR = By.id("newForm:year_input");
-	public static final By INPUT_PASSWORD_EXPIRE = By.id("newForm:passex_input");
+	public static final By INPUT_PASSWORD_EXPIRES = By.id("newForm:passex_input");
 	public static final String BASE_SELECT_ROLES = "newForm:roles";
 	public static final String BASE_SELECT_STATUS = "newForm:status";
-	public static final By INPUT_EMAIL = By.id("newForm:email");
+	public static final By INPUT_EMAIL = By.xpath("//*[@id=\"newForm:j_idt494_display\"]");
+	
 	public static final By INPUT_DAYTIME_PHONE = By.id("newForm:dayPhone");
 	public static final By TYPE_ADDRESS = By.id("newForm:area");
 	public static final By CREATE_USER_BUTTON = By.id("newForm:createBtn");
-	public static final By BUTTON_NEW_USER_PANEL = By.id("butForm:newUserBtn");
+	public static final By BUTTON_NEW_USER_PANEL = By.xpath("//*[@id=\"butForm:newUserBtn\"]/span[2]");
 
 	// SHOW USERS
 	public static final By BUTTON_SHOW_USERS = By.xpath("//div[@id='butForm:showUsersBtn']/span");
@@ -48,3 +61,4 @@ public class UserProfilesPage {
 			.xpath("//div[@id='usersForm:usersTable:setStatusTo_menu']/ul/li[4]/a/span");
 
 }
+
